d817d92de5e70cf9ab1375f413f3c361f7ec713a
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-02-04 12:47:11 +0530

    Back_End Features of login added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,14 +42,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
  config.action_mailer.perform_deliveries = true
- config.action_mailer.delivery_method = :sendmail
  config.action_mailer.default_options = {from: 'anish.kumbhar04@gmail.com'}
 config.action_mailer.smtp_settings = {
 address:              'smtp.gmail.com',
 port:                 587,
 domain:               'gmail.com',
-#user_name:            Rails.application.credentials.gmail[:email],
-#password:            Rails.application.credentials.gmail[:password],
+user_name:            'anish.kumbhar04@gmail.com',
+password:            'gbtkcryoarmpsgxu',
 authentication:       'plain',
 enable_starttls_auto: true  }
 config.action_mailer.default_url_op
