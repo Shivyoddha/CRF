f5a85112f0aa57d68da98602ee4f8d5c193569b3
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-02-04 16:19:38 +0530

    Back-End support to Register page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_000045) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_04_104855) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_000045) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "department"
+    t.string "course"
+    t.string "orgname"
+    t.string "orgaddress"
+    t.binary "collegeid"
+    t.string "profession"
+    t.string "rollno"
+    t.string "contact"
+    t.string "lastname"
+    t.string "firstname"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
