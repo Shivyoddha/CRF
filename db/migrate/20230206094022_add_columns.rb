8b1b61dc7d5103eaa593a57799249461d27b7ab3
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-02-06 15:20:57 +0530

    Editable forms

---
+++ db/migrate/20230206094022_add_columns.rb
@@ -0,0 +1,13 @@
+class AddColumns < ActiveRecord::Migration[7.0]
+  def change
+    add_column :equipment, :name, :string
+    add_column :equipment, :status, :string
+    add_column :equipment, :feature, :text
+    add_column :equipment, :resolution, :text
+    add_column :equipment, :location, :text
+    add_column :equipment, :application, :text
+    add_column :equipment, :facilities, :text
+    add_column :questions, :content, :text
+    add_column :questions, :type, :string
+  end
+end
