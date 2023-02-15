df17c185785640e95274074d24b941f0f45568e5
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-02-16 00:12:02 +0530

    Authorization added

--- app/controllers/admin_portal_controller.rb
+++ app/controllers/admin_portal_controller.rb
@@ -1,5 +1,21 @@
 class AdminPortalController < ApplicationController
+
   def admindashboard
-      
+
+
+  if user_signed_in?
+
+    @user=User.find(current_user.id)
+   if @user.admin_role?
+   else
+     redirect_to home_index_path
+   end
+
+  else
+     redirect_to new_user_session_path
+
+  end
+
+
   end
 end
