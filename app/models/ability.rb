bad716b10269f674653029daf1b4f9cf7705384a
Nikesh <nikeshs129@gmail.com>
2023-02-20 20:54:36 +0530

    Slotbooker controller added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,15 +4,17 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-
     if user.admin_role?
       can :manage, :all
       can :access, :slotbooker
       can :access, :rails_admin  # only admin can access Rails Admin
       can :manage, :dashboard
+
+    end
  # allow access to dashboard
 
-else if user.chairman_role?
+
+    if user.chairman_role?
      can :access, :rails_admin
 
       can :read, :all
