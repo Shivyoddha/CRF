7ad62d688db1a9928cab9a1e91fe2a43deff7aae
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-02-21 01:29:18 +0530

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_20_184450) do
+ActiveRecord::Schema.define(version: 2023_02_20_195500) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -315,6 +315,7 @@ ActiveRecord::Schema.define(version: 2023_02_20_184450) do
     t.boolean "chairman_role"
     t.string "name"
     t.string "slotbooker"
+    t.string "firstname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
