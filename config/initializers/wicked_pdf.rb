dfc1b0b2ffd01e439d3b62ea783e8031bd707764
Preetham <preethamgr123@gmail.com>
2023-02-21 01:07:09 +0530

    issue sorted

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -14,7 +14,7 @@ WickedPdf.config = {
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-  :exe_path =>'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe',
+  :exe_path =>'"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
     enable_local_file_access: true
   # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
   # enable_local_file_access: true,
