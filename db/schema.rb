e8790ce1549a8797e64c917c89aa9a0f8790be30
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-02-22 02:25:59 +0530

    xrd new changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_20_223721) do
+ActiveRecord::Schema.define(version: 2023_02_21_201947) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -346,6 +346,7 @@ ActiveRecord::Schema.define(version: 2023_02_20_223721) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "debit"
     t.integer "user_id"
+    t.string "status"
     t.index ["user_id"], name: "index_xrds_on_user_id"
   end
 
