d11ea3b69c508263d49677b98e6d0c052d6457b9
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-06-24 17:21:56 +0530

    anish1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-
+    can :manage, :rails_admin
 
     if user.developer? # Assuming there is a 'developer' boolean attribute on the User model
     can :access, :rails_admin       # grant access to rails_admin
