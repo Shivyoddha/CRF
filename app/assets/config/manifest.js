940c4d426bf9b282365d5a15f24977bb81e7083f
Preetham Naik <99126947+PreethamNaik12@users.noreply.github.com>
2024-03-05 10:41:23 +0000

    fix: - home/index page view Navbar - search bar responsiveness - optimize search bar

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 
-  //= link Navbar/new_nav.css
-    //= link Navbar/new_navResp.css
+//= link Navbar/logoNav.css
+//= link Navbar/new_nav.css
+//= link Navbar/searchBar.css
+//= link Navbar/new_navResp.css
